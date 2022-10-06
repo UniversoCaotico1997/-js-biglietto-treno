4d97572b9b3175f7af88f8c3171a5d5a022dbe1d
UniversoCaotico1997 <loria.roberto97@gmail.com>
2022-10-06 16:05:17 +0200

    PrezzoInBaseAiKm

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,4 +23,20 @@
 const currentAge = prompt(`Quanti anni hai?`)
 console.log(currentAge);
 
+
+
+// il prezzo del biglietto è definito in base ai km (0.21 € al km)
+
+const prezzoInBaseAiKm = (`0.21`)
+console.log(prezzoInBaseAiKm);
+
+
+
+// va applicato uno sconto del 20% per i minorenni
+
+
+
+// va applicato uno sconto del 40% per gli over 65.
+
+
 // Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio
