c4095015f6ce31a64124479f6177d6de649f7678
UniversoCaotico1997 <loria.roberto97@gmail.com>
2022-10-06 18:10:03 +0200

    Finish js-biglietto-treno

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,6 @@
 // va applicato uno sconto del 20% per i minorenni
 // va applicato uno sconto del 40% per gli over 65.
 // L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
-// Questo richiederà un minimo di ricerca, oltre alla ricerca da fare per ricordarsi come calcolare uno sconto (Googlate qulcosa del tipo: come si calcola lo sconto? e cercate una risorsa che vi possa aiutare a capire cosa fare)
 
 
 
@@ -14,41 +13,57 @@
 
 
 //  il numero di chilometri che vuole percorrere
+
+
  
- const chilometriDaPercorrere = prompt(`Qaunti chilometri vuoi percorere?`);
+ const chilometriDaPercorrere = prompt (`Qaunti chilometri vuoi percorere?`);
  console.log(chilometriDaPercorrere);
+
+
 
 // e l'età del passeggero.
 
-const currentAge = prompt(`Quanti anni hai?`);
-console.log(currentAge);
 
+const currentAge = prompt (`Quanti anni hai?`);
+console.log(currentAge);
 
 
 // il prezzo del biglietto è definito in base ai km (0.21 € al km)
 
-const tariffaInBaseAiKm = (`0.21`);
+
+const tariffaInBaseAiKm = 0.21;
 console.log(tariffaInBaseAiKm);
 
-const prezzoInBaseAiKmPercorsi = `chilometriDaPercorrere` + `tariffaInBaseAiKm`;
+const prezzoInBaseAiKmPercorsi = chilometriDaPercorrere * tariffaInBaseAiKm;
 console.log(prezzoInBaseAiKmPercorsi);
 
 
 // va applicato uno sconto del 20% per i minorenni
 
-const under18 = (`< 18`);
-console.log(under18);
 
-const percentualeMinima = (`20%`);
+const percentualeMinima = 20;
 console.log(percentualeMinima);
+
+const totaleMinimo = (prezzoInBaseAiKmPercorsi / 100) * percentualeMinima;
+console.log(totaleMinimo);
 
 
 // va applicato uno sconto del 40% per gli over 65.
 
-const over65 = (`> 65`);
-console.log(over65);
 
-const percentualeMassima = (`40%`);
+const percentualeMassima = 40;
 console.log(percentualeMassima);
 
+const totaleMassimo = (prezzoInBaseAiKmPercorsi / 100) * percentualeMassima;
+console.log(totaleMassimo);
+
+
 // Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio
+
+if (currentAge < 18) {
+    console.log(totaleMinimo);
+} else if (currentAge > 65) {
+    console.log(totaleMassimo);
+} else {
+    console.log(prezzoInBaseAiKmPercorsi);
+}
